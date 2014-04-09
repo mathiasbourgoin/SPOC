ae45a1b88deb53731b1c56cf4421b5dde0e04538
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2014-04-09 09:45:38 +0200

    fix2

--- SpocLibs/Sarek/Kirc.mli
+++ SpocLibs/Sarek/Kirc.mli
@@ -106,8 +106,6 @@ val return_unit : unit -> Kirc_Ast.k_ext
 val return_int : int -> Kirc_Ast.k_ext
 val return_float : int -> Kirc_Ast.k_ext
 val return_double : int -> Kirc_Ast.k_ext
-val print : string -> unit
-val debug_print : ('a, 'b, 'c, 'd, 'e) sarek_kernel -> unit
 val rewrite : Kirc_Ast.k_ext -> Kirc_Ast.k_ext
 val return_v : (string * string) ref
 val save : string -> string -> unit
@@ -116,7 +114,8 @@ val gen :
   ?return:bool ->
   ?only:Spoc.Devices.specificLibrary ->
   ('a, 'b, 'c, 'd, 'e) sarek_kernel ->
-  ('a, 'b) Spoc.Kernel.spoc_kernel * ('c, 'd, 'e) kirc_kernel
+  ('a, 'b, 'c, 'd, 'e) sarek_kernel 
+
 val run :
   ?recompile:bool ->
   ('a, 'b, 'c, 'd, 'e) sarek_kernel ->
