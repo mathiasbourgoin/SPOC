906637ea9bc092c99bfdee1ed2544f5380c7f525
mathias <mathias.bourgoin@gmail.com>
2015-10-07 16:16:19 +0200

    makefile change

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -26,7 +26,7 @@ opam --git-version
 opam init
 eval `opam config env`
 case $OCAML_VERSION in
-    4.02) opam install camlp4;;
+    4.02|4.02.3) opam install camlp4;;
 esac
 #opam install ocp-build ctypes
 opam install ctypes
