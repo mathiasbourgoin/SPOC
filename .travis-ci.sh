ef3f41235fe9a5d1b6d9364313a9c4e22215a262
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2016-09-13 11:12:34 +0200

    travis-ci install ocaml_fastflow

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,7 +15,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam 
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam g++ libffi-dev
 
 export OPAMYES=1
 export OPAMVERBOSE=1
