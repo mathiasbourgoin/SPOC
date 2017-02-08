ac14d7ba45b457c1f02858a5522ff7d5ca8a23d8
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2017-02-08 17:39:05 +0100

    closes #35

--- Samples/src/CustomPi/CustomPi.ml
+++ Samples/src/CustomPi/CustomPi.ml
@@ -11,18 +11,31 @@ external getsizeofpoint: unit -> int = "custom_getsizeofpoint"
 external extget: customarray -> int -> point = "custom_extget"
 external extset: customarray -> int -> point -> unit = "custom_extset"
 
+
+external getsizeofpoint2: unit -> int = "custom_getsizeofpointdouble"
+external extget2: customarray -> int -> point = "custom_extgetdouble"
+external extset2: customarray -> int -> point -> unit = "custom_extsetdouble"
+
 let customPoint = {
   size = getsizeofpoint();
   get = extget;
   set = extset;
 }
 
+let customPoint2 = {
+  size = getsizeofpoint2();
+  get = extget2;
+  set = extset2;
+}
+
 let nbPoint = 2_000_000;;
 let ray = 10.0;;
 
 
-kernel gpuPi : point Spoc.Vector.vcustom -> Spoc.Vector.vfloat32 -> int -> float -> unit = "kernels/CustomPi" "pi"
- 
+kernel gpuPi : point Spoc.Vector.vcustom -> Spoc.Vector.vint32 -> int -> float -> unit = "kernels/CustomPi" "pi"
+
+kernel gpuPi_double : point Spoc.Vector.vcustom -> Spoc.Vector.vint32 -> int -> float -> unit = "kernels/CustomPi" "pi_double"
+
 
 
 let cpuPI field size =
@@ -32,13 +45,22 @@ let cpuPI field size =
   let res = Array.fold_left (fun i res -> if res then i + 1 else i) 0 resVec
   in res
 
+let gpuPi_d dev (block,grid) (gpuField, vbool, nbPoint, ray)=
+    if Spoc.Devices.allowDouble dev then
+    Spoc.Kernel.run dev (block, grid) gpuPi_double
+       (gpuField, vbool, nbPoint, (ray *. ray))
+  else
+    Spoc.Kernel.run dev (block, grid) gpuPi
+       (gpuField, vbool, nbPoint, (ray *. ray))
+;;
+
 let gpuPI gpuField vbool dev size =
   let threadsPerBlock =  match dev.Devices.specific_info with
-          | Devices.OpenCLInfo clI -> 
-            (match clI.Devices.device_type with
-              | Devices.CL_DEVICE_TYPE_CPU -> 1
-              | _  ->   256)
-          | _  -> 256 in
+    | Devices.OpenCLInfo clI -> 
+      (match clI.Devices.device_type with
+       | Devices.CL_DEVICE_TYPE_CPU -> 1
+       | _  ->   256)
+    | _  -> 256 in
   let blocksPerGrid =
     (((size / 32) + threadsPerBlock) - 1) / threadsPerBlock in
   let block =
@@ -54,12 +76,12 @@ let gpuPI gpuField vbool dev size =
       Spoc.Kernel.gridZ = 1;
     }
   in
-  (Spoc.Kernel.run dev (block, grid) gpuPi
-      (gpuField, vbool, nbPoint, (ray *. ray));
-    let pio4 =
-      int_of_float
-        (Tools.fold_left (fun a b -> a +. b) 0. vbool)
-    in pio4)
+  gpuPi_d dev (block, grid) (gpuField, vbool, nbPoint, ray);
+  
+  let pio4 =
+    Int32.to_int
+      (Tools.fold_left (fun a b -> Int32.add a b) Int32.zero vbool)
+  in pio4;;
 
 let multiGpuPI gpuField1 gpuField2 vbool1 vbool2 dev1 dev2 size =
   let threadsPerBlock = 512 in
@@ -78,29 +100,30 @@ let multiGpuPI gpuField1 gpuField2 vbool1 vbool2 dev1 dev2 size =
       Spoc.Kernel.gridZ = 1;
     }
   in
-  (Spoc.Kernel.run dev1 (block, grid) gpuPi
-      (gpuField1, vbool1, (size / 2), (ray *. ray));
-    Spoc.Kernel.run dev2 (block, grid) gpuPi
-      (gpuField2, vbool2, (size / 2), (ray *. ray));
-    let pio4_1 =
-      int_of_float
-        (Tools.fold_left (fun a b -> a +. b) 0. vbool1)
-    and pio4_2 =
-      int_of_float
-        (Tools.fold_left (fun a b -> a +. b) 0. vbool2)
-    in
-    let pio4 = pio4_1 + pio4_2 in pio4)
-
+  (gpuPi_d dev1 (block, grid)(gpuField1, vbool1, (size / 2), ray);
+  gpuPi_d dev2 (block, grid)(gpuField1, vbool2, (size / 2), ray);
+  let pio4_1 =
+    Int32.to_int
+      (Tools.fold_left (fun a b -> Int32.add a  b) Int32.zero vbool1)
+  and pio4_2 =
+    Int32.to_int
+      (Tools.fold_left (fun a b -> Int32.add a b) 0l vbool2)
+  in
+  let pio4 = pio4_1 + pio4_2 in pio4)
+;;
 let _ =
   (Random.self_init ();
-      let devices = Spoc.Devices.init () in
-      let dev = ref devices.(0) in
-      let size = ref nbPoint in
-      let arg1 = ("-device" , Arg.Int (fun i  -> dev := devices.(i)), "number of the device [0]")
-      and arg2 = ("-size" , Arg.Int (fun i  -> size := i), "width of the image to compute [2_000_000]")
-      in
-      Arg.parse ([arg1;arg2;]) (fun s -> ()) "";
-
+   let devices = Spoc.Devices.init () in
+   let dev = ref devices.(0) in
+   let size = ref nbPoint in
+   let arg1 = ("-device" , Arg.Int (fun i  -> dev := devices.(i)), "number of the device [0]")
+   and arg2 = ("-size" , Arg.Int (fun i  -> size := i), "width of the image to compute [2_000_000]")
+   in
+   Arg.parse ([arg1;arg2;]) (fun s -> ()) "";
+   if Devices.allowDouble !dev then
+     Printf.printf "Will use double precision\n"
+   else
+     Printf.printf "Will use simple precision, results may be innacurate ;-)\n";
     let pio4 = ref 0 in (*CPU COMPUTATION*)
     let t0 = Unix.gettimeofday () in
     let field =
@@ -116,16 +139,21 @@ let _ =
         Printf.printf "Will use device : %s\n" !dev.Spoc.Devices.general_info.Spoc.Devices.name;
         Spoc.Kernel.compile !dev gpuPi;
         Spoc.Mem.auto_transfers false;
-        let vbool = Spoc.Vector.create float32 !size in
+        let vbool = Spoc.Vector.create int32 !size in
         let gpuField =
-          Spoc.Tools.map
-            (fun _ -> { x = Random.float ray; y = Random.float ray; })
-            (Custom customPoint) vbool
+          if Devices.allowDouble !dev then
+            Spoc.Tools.map
+              (fun _ -> { x = Random.float ray; y = Random.float ray; })
+              (Custom customPoint2) vbool
+          else
+            Spoc.Tools.map
+              (fun _ -> { x = Random.float ray; y = Random.float ray; })
+              (Custom customPoint) vbool
         in
         (Spoc.Mem.to_device gpuField ~queue_id: 1 !dev;
           Spoc.Mem.to_device vbool ~queue_id: 0 !dev ;
           pio4 := gpuPI gpuField vbool !dev !size;
-          Printf.printf "GPU Computation : PI = %d/%d = %.10G\n" !pio4 !size
+          Printf.printf "GPU Computation : PI = %d/%d = %.10g\n" !pio4 !size
             ((4. *. (float !pio4)) /. (float !size));
           Pervasives.flush stdout;
         ))))
