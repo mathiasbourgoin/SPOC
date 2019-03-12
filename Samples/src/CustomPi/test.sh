244358bac12b60b83ce5b3daf90ac3284c3246f4
MBourgoin <mathias.bourgoin@gmail.com>
2019-03-12 14:22:43 +0100

    Sample/CustomPi: Fix test

--- Samples/src/CustomPi/test.sh
+++ Samples/src/CustomPi/test.sh
@@ -6,7 +6,7 @@ printf "\n  + GPU_COMPUTATION:"
 
 res1=`./${1} grep "GPU Computation" ${res} | tr -d "[:alpha:]" | tr -d ":"`
 
-if `echo "${res1}" | grep -q 4\.` ; then
+if `echo "${res1}" | grep -q 3\.` ; then
     printf "${OK}\n"
 else
     printf "${KO}\n"
@@ -17,7 +17,7 @@ printf "  + GPU_COMPUTATION WITH COMPLEX32:"
 res2=`./${1} grep "GPU Complex" ${res} | tr -d "[:alpha:]" | tr -d ":" `
 
 
-if `echo "${res2}" | grep -q 4\.` ; then
+if `echo "${res2}" | grep -q 3\.` ; then
     printf "${OK}\n"
 else
     printf "${KO}\n"
